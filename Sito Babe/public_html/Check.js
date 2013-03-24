e13ae5bb70ee746ef28cdfac22f6c0fb986aad74
Andrea Meneghinello <meneghinello.andrea@gmail.com>
2013-03-24 16:41:37 +0100

    Inserita seconda funzione js

--- Sito Babe/public_html/Check.js
+++ Sito Babe/public_html/Check.js
@@ -1,6 +1,17 @@
-function checkEmail()
+function checkEmail(idElement, idError)
 {
-	
+	 var re =/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	 var email = document.getElementById(idElement).value;
+
+    if (!re.test(email))
+	{
+		document.getElementById(idError).style.display = "inline";
+		return true;
+	}
+	else
+	{
+		document.getElementById(idError).style.display = "none";
+	}
 }
 
 function checkText(idElement, idError)
